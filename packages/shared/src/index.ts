f8a3774b753f605b189bcbc4a8feda8b5af426a3
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-01 13:09:06 -0500

    feat(backend/shared): add central export hub for DTOs and interfaces
    - Created index.ts within shared module to export message related DTOs
    - Included all offer related DTOs and interfaces in the shared index file

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,7 @@
+export { ICreateMessageDto } from './message/dto/create-message.dto';
+export { IUpdateMessageDto } from './message/dto/update-message.dto';
+export { IMessage } from './message/message';
+
+export { ICreateOfferDto } from './offer/dto/create-offer.dto';
+export { IUpdateOfferDto } from './offer/dto/update-offer.dto';
+export { IOffer } from './offer/offer';
