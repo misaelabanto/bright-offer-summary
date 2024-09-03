6e5e2ea1094dd838e8cba00089f2b07f34dcad9f
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-02 20:42:42 -0500

    feat(backend): enhance messaging module with WhatsApp config
    - Added HttpModule to app.module.ts for http-related functionalities
    - Imported ConfigModule in messaging.module.ts for WhatsApp config setup
    - Integrated WhatsApp config in messaging.module.ts for better config management

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MessageApiModule } from '~/api/message/message-api.module';
 import { StatusApiModule } from '~/api/status/status-api.module';
+import { HttpModule } from '~/http/http.module';
 import { MessageModule } from '~/message/message.module';
 import { MessagingModule } from '~/messaging/messaging.module';
 import { OfferModule } from '~/offer/offer.module';
 
 @Module({
 	imports: [
+		HttpModule,
 		MessageModule,
 		OfferModule,
 		MessagingModule,
--- apps/backend/src/messaging/messaging.module.ts
+++ apps/backend/src/messaging/messaging.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import whatsappConfig from '~/common/config/whatsapp.config';
 import { WhatsAppMessagingProvider } from '~/messaging/providers/whatsapp.provider';
 
 @Module({
+	imports: [ConfigModule.forFeature(whatsappConfig)],
 	providers: [WhatsAppMessagingProvider],
 })
 export class MessagingModule {}
