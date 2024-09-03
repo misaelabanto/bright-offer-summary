c4ac0d943a39be60191115c22cee822643202322
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-02 22:35:12 -0500

    feat(frontend/home): add HomePage component for navigation links
    - Created HomePage component in home/page.tsx
    - Added navigation links to Schedule Message, Messages List, and About
    - Styled links with underline and hover effect decorations

---
+++ apps/frontend/src/home/page.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+export const HomePage: FC = () => {
+	return (
+		<main className="flex justify-center items-center h-screen">
+			<div className="flex flex-col gap-10 grid-col-span-1 text-3xl">
+				<Link className="underline hover:decoration-blue-500" to="/message">
+					Schedule Message
+				</Link>
+				<Link className="underline hover:decoration-blue-500" to="/message/:id">
+					Messages List
+				</Link>
+				<Link className="underline hover:decoration-blue-500" to="/about">
+					About
+				</Link>
+			</div>
+		</main>
+	);
+};
