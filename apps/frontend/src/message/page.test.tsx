dc83f4b0de6b1db059460bcbf378447c76aedc4a
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-04 10:35:36 -0500

    feat(frontend): add tsconfig path mapping in vitest config
    - Added vite-tsconfig-paths plugin to vitest.config.ts
    - Improved module resolution to align with TypeScript paths configuration
    
    feat(frontend/message): add tests for MessagesPage and update vitest config
    
    - Created tests for loading state and display of messages in MessagesPage
    - Mocked useMessages hook in tests for MessagesPage
    - Added vite-tsconfig-paths plugin in vitest.config.ts
    - Improved module resolution to align with TypeScript paths configuration

---
+++ apps/frontend/src/message/page.test.tsx
@@ -0,0 +1,52 @@
+import { useMessages } from '@/message/hooks/use-messages';
+import { IMessage, IOffer } from '@bright-offer-summary/shared';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Mock } from 'vitest';
+import { MessagesPage } from './page';
+
+// Mock the useMessages hook
+vi.mock('@/message/hooks/use-messages');
+
+describe('MessagesPage', () => {
+	it('displays loading state when messages are null', () => {
+		// Mock the return value of useMessages
+		(useMessages as Mock).mockReturnValue({ messages: null });
+
+		render(<MessagesPage />);
+
+		// Check if the loading text is displayed
+		expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
+	});
+
+	it('displays MessagesTable when messages are available', () => {
+		const mockMessages: IMessage[] = [
+			{
+				id: '1',
+				createdAt: new Date(),
+				offer: {} as IOffer,
+				phoneNumber: '1234567890',
+				sendAt: new Date(),
+				status: 'pending',
+			},
+			{
+				id: '1',
+				createdAt: new Date(),
+				offer: {} as IOffer,
+				phoneNumber: '1234567890',
+				sendAt: new Date(),
+				status: 'sent',
+			},
+		];
+
+		// Mock the return value of useMessages
+		(useMessages as Mock).mockReturnValue({ messages: mockMessages });
+
+		render(<MessagesPage />);
+
+		// Check if the MessagesTable is displayed
+		expect(screen.getByText(/Scheduled messages/i)).toBeInTheDocument();
+		expect(screen.getByText(/pending/i)).toBeInTheDocument();
+		expect(screen.getByText(/sent/i)).toBeInTheDocument();
+	});
+});
