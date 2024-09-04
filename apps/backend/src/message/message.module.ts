41565c2e477e43d986f654695135622349958d7e
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-03 23:57:40 -0500

    feat(backend/message): integrate fake messaging provider support
    -The MessagingModule is now imported in MessageModule for better cohesion.
    -Added FakeMessagingProvider to MessagingModule to facilitate testing.
    -FakeMessagingProvider and WhatsAppMessagingProvider are now exported.

--- apps/backend/src/message/message.module.ts
+++ apps/backend/src/message/message.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import databaseConfig from '~/common/config/database.config';
 import frontendConfig from '~/common/config/frontend.config';
 import { MessageService } from '~/message/message.service';
+import { MessagingModule } from '~/messaging/messaging.module';
 import { OfferModule } from '~/offer/offer.module';
 
 @Module({
 	imports: [
+		MessagingModule,
 		OfferModule,
 		ConfigModule.forFeature(databaseConfig),
 		ConfigModule.forFeature(frontendConfig),
--- apps/backend/src/messaging/messaging.module.ts
+++ apps/backend/src/messaging/messaging.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import whatsappConfig from '~/common/config/whatsapp.config';
+import { FakeMessagingProvider } from '~/messaging/providers/fake.provider';
 import { WhatsAppMessagingProvider } from '~/messaging/providers/whatsapp.provider';
 
 @Module({
 	imports: [ConfigModule.forFeature(whatsappConfig)],
-	providers: [WhatsAppMessagingProvider],
+	providers: [FakeMessagingProvider, WhatsAppMessagingProvider],
+	exports: [FakeMessagingProvider, WhatsAppMessagingProvider],
 })
 export class MessagingModule {}
