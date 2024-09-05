90fab168e1035acc674283a52c169e6b91901a35
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-04 21:15:46 -0500

    test(frontend/message): add unit tests for message hooks
    - Added tests for useCreateMessage to verify initial state, fetch function
      call, and state updates
    - Added tests for useMessage to validate successful fetch, error handling,
      and loading state
    - Added tests for useMessages to check successful fetch, error handling,
      and loading state

---
+++ apps/frontend/src/message/hooks/use-message.test.ts
@@ -0,0 +1,60 @@
+import { useMessage } from '@/message/hooks/use-message';
+import { IMessage, IOffer } from '@bright-offer-summary/shared';
+import { renderHook } from '@testing-library/react-hooks';
+import useSWR from 'swr';
+import { Mock, vi } from 'vitest';
+
+vi.mock('swr');
+
+describe('useMessage', () => {
+	it('should return message data when fetch is successful', async () => {
+		const mockMessage: IMessage = {
+			id: '1',
+			offer: {} as IOffer,
+			phoneNumber: '123456789',
+			sendAt: new Date(),
+			createdAt: new Date(),
+			status: 'sent',
+		};
+		(useSWR as Mock).mockReturnValue({
+			data: mockMessage,
+			error: null,
+			isLoading: false,
+		});
+
+		const { result } = renderHook(() => useMessage('1'));
+
+		expect(result.current.message).toEqual(mockMessage);
+		expect(result.current.error).toBeNull();
+		expect(result.current.isLoading).toBe(false);
+	});
+
+	it('should return an error when fetch fails', async () => {
+		const mockError = new Error('Failed to fetch');
+		(useSWR as Mock).mockReturnValue({
+			data: null,
+			error: mockError,
+			isLoading: false,
+		});
+
+		const { result } = renderHook(() => useMessage('1'));
+
+		expect(result.current.message).toBeNull();
+		expect(result.current.error).toEqual(mockError);
+		expect(result.current.isLoading).toBe(false);
+	});
+
+	it('should indicate loading state correctly', async () => {
+		(useSWR as Mock).mockReturnValue({
+			data: null,
+			error: null,
+			isLoading: true,
+		});
+
+		const { result } = renderHook(() => useMessage('1'));
+
+		expect(result.current.message).toBeNull();
+		expect(result.current.error).toBeNull();
+		expect(result.current.isLoading).toBe(true);
+	});
+});
