31a922cb284ef8a888a555b596c47661d7eef730
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-05 19:19:23 -0500

    feat(frontend/message): improve message scheduling form UX
    - Added toast notifications for success and error responses
    - Enhanced validation error handling with detailed messages
    - Included a button to view all messages after scheduling

--- apps/frontend/src/message/schedule/page.tsx
+++ apps/frontend/src/message/schedule/page.tsx
@@ -1,7 +1,9 @@
 import { useCreateMessage } from '@/message/hooks/use-create-message';
 import { MessageForm } from '@/message/schedule/message-form';
 import { ICreateMessageDto } from '@bright-offer-summary/shared';
+import { Button } from '@bright-offer-summary/ui';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 export const MessageSchedulePage: FC = () => {
 	const { trigger: createMessage, isLoading } = useCreateMessage();
@@ -14,6 +16,11 @@ export const MessageSchedulePage: FC = () => {
 		<div className="p-10">
 			<h1 className="text-4xl">Schedule message</h1>
 			<MessageForm onSubmit={onFormSubmit} isLoading={isLoading} />
+			<Link to="/admin/messages">
+				<Button type={'submit'}>
+					<span>View all messages</span>
+				</Button>
+			</Link>
 		</div>
 	);
 };
