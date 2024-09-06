042bfe15bde4e539980d7b0d091d3e088b80830c
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-06 18:51:11 -0500

    feat(backend/api/status): add ApiOkResponse to status endpoint
    - Added ApiOkResponse decorator to status controller getStatus method
    - This enhances the Swagger documentation with response type info

--- apps/backend/src/api/status/status.controller.ts
+++ apps/backend/src/api/status/status.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { StatusDto } from '~/api/status/dto/status.dto';
 
 @Controller('status')
 @ApiTags('status')
 export class StatusController {
 	@Get()
+	@ApiOkResponse({ type: StatusDto })
 	getStatus(): Promise<StatusDto> {
 		return Promise.resolve({ status: 'ok' });
 	}
