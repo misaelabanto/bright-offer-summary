70ee09a55c2e10232badf087f4a58c61ba7c255d
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-05 19:23:13 -0500

    fix(frontend/admin): improve layout and styling of admin page
    - Aligned grid columns for better responsiveness
    - Added fixed width to cards
    - Enhanced icon styling with hover effect to indicate interactivity

--- apps/frontend/src/admin/page.tsx
+++ apps/frontend/src/admin/page.tsx
@@ -6,17 +6,17 @@ export const AdminPage: FC = () => {
 	return (
 		<>
 			<h1 className="text-4xl text-center">Admin Portal</h1>
-			<div className="flex justify-center items-center h-screen">
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-10 text-3xl">
+			<div className="flex justify-center items-center ">
+				<div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
 					<Link
 						to="messages"
-						className="card bg-base-100 shadow-lg hover:shadow-2xl"
+						className="card w-80 bg-base-100 shadow-lg hover:shadow-2xl"
 					>
 						<div className="card-body flex items-center">
 							<Icon
 								icon="mdi:calendar-clock"
 								fontSize={48}
-								className="text-primary"
+								className="text-primary hover:text-secondary"
 							/>
 							<h2 className="card-title">View Scheduled Messages</h2>
 							<p>Access the list of messages that are scheduled to be sent.</p>
@@ -24,13 +24,13 @@ export const AdminPage: FC = () => {
 					</Link>
 					<Link
 						to="/admin/messages/schedule"
-						className="card bg-base-100 shadow-lg hover:shadow-2xl"
+						className="card w-80 bg-base-100 shadow-lg hover:shadow-2xl"
 					>
 						<div className="card-body flex items-center">
 							<Icon
 								icon="mdi:calendar-plus"
 								fontSize={48}
-								className="text-primary"
+								className="text-primary hover:text-secondary"
 							/>
 							<h2 className="card-title">Schedule New Message</h2>
 							<p>Schedule a new message to be sent at a specified time.</p>
